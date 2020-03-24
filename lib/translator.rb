69977ea515fa46762f094993701197e20b952b2c
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-24 20:44:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,11 +4,11 @@ require "pry"
 def load_library(file_path)
   library = YAML.load_file(file_path)
   result = {
-      :get_meaning => { },
+      :get_meaning => [ ],
       :get_emoticon => {}
     }
  library.each do | key, value |
-   result[:get_meaning] = value[1] 
+   result[:get_meaning] = [ ] << value[1] 
  end
    #binding.pry
 return result
