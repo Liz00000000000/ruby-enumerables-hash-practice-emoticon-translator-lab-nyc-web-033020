921b9aa3fa280b0aeebc7f587c90a82ed6c29512
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-24 21:20:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,7 @@ def get_japanese_emoticon(file_path, emoticon)
     return data[:get_emoticon][emoticon]
   else
     return "Sorry, that emoticon was not found"
+  end
 end
 
 def get_english_meaning
