54feb9d3d4e89f774780fa44934c9ae00642b25f
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-24 20:00:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,8 @@ def load_library(file_path)
       :get_emoticon => {}
     }
  library.each do | key, value |
-   result[:get_meaning] = key => {value[0] => value[1]}}
+   result[:get_meaning] = key
+   result[:get_emoticon]=> {value[0] => value[1]}}
    #binding.pry
 end
 return result
